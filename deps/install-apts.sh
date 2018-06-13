3486722536f5d2310f83e077520ea6767ebfa629
Feng Li <fengggli@yahoo.com>
2018-06-13 10:02:17 -0700

    try add headers in both host and guest

--- deps/install-apts.sh
+++ deps/install-apts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y wget git gcc libpciaccess-dev make libcunit1-dev pkg-config \
+  linux-headers-`uname -r`
 # optional
 #
 # apt-get install emacs elpa-company 
