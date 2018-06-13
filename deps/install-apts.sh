f6daf929be8d09a0e0c992bac930ea333b6d2b0f
Feng Li <fengggli@yahoo.com>
2018-06-13 09:34:17 -0700

    add bootstrap file

---
+++ deps/install-apts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update
+apt-get install -y wget git gcc libpciaccess-dev make libcunit1-dev pkg-config \
+# optional
+#
+# apt-get install emacs elpa-company 
+#
---
+++ deps/install-yum.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# build-essentials for fedora
+yum -y install wget git make automake gcc-c++ openssl-devel sqlite-devel 
